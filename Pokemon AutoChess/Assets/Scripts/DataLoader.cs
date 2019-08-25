20dc666f62d3d605b229b5ae34d83f35b6dd1e24
DariusChoo <dariuschoo@gmail.com>
2019-08-26 00:13:22 +0800

    Added Dataloader Script
    -Using PHP, can retrieve data from database
    **NOTE**
    This will not work for the rest of you because I'm using localhost (data stored on local machine)
    I will see how to host it on github url like WCD and update in the future

---
+++ Pokemon AutoChess/Assets/Scripts/DataLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour
+{
+    //Container to keep data
+    public string[] units;
+
+    //Point to where to start retrieving data
+    //E.G: (Unit Name:)
+    public string index2;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        //Extracts data from this page
+        WWW UnitData = new WWW("http://localhost/PokeChess/UnitData.php");
+
+        //Waits for downloading to finish before running further code
+        yield return UnitData;
+
+        //Displays all data retrieved in Console
+        string UnitDataString = UnitData.text;
+        print(UnitDataString);
+
+        //Splits said data by semi colon - so it's separated by each row
+        units = UnitDataString.Split(';');
+
+
+        print(GetDataValue(units[0], index2));
+    }
+
+    string GetDataValue(string data, string index)
+    {
+        //First it gets the whole row of record in string form
+        //Next it cuts it to a substring starting with index
+        //Then it cuts off everything else from the point where | begins
+        //Returns remainder substring
+        string value = data.Substring(data.IndexOf(index) + index.Length);
+        value = value.Remove(value.IndexOf("|"));
+        return value;
+    }
+}
