1d9493513f9da327385fd2affc80d7aa75e6e7ac
DariusChoo <dariuschoo@gmail.com>
2019-08-26 21:49:31 +0800

    Updated DataLoader
    - When assigned to gameobject with tag, it will check if the name is in the database and retrieve stats

--- Pokemon AutoChess/Assets/Scripts/DataLoader.cs
+++ Pokemon AutoChess/Assets/Scripts/DataLoader.cs
@@ -9,7 +9,29 @@ public class DataLoader : MonoBehaviour
 
     //Point to where to start retrieving data
     //E.G: (Unit Name:)
-    public string index2;
+    string getName = "Unit Name:";
+    string getCost = "Unit Cost:";
+    string getSynergy = "Unit Synergy:";
+    string getSynergy2 = "Unit Second Synergy:";
+    string getHP = "Unit Health Points:";
+    string getMana = "Unit Mana Points:";
+    string getAbility = "Unit Ability:";
+    string getAttackSpeed = "Unit Attack Speed:";
+    string getDamage = "Unit Damage:";
+    string getArmor = "Unit Armor:";
+    string name = "";
+
+    //Variables to get stats
+    public string unitName;
+    public int cost;
+    public string synergy;
+    public string synergy2;
+    public int HP;
+    public int mana;
+    public string ability;
+    public int attackSpeed;
+    public int damage;
+    public int armor;
 
     // Start is called before the first frame update
     IEnumerator Start()
@@ -26,9 +48,8 @@ public class DataLoader : MonoBehaviour
 
         //Splits said data by semi colon - so it's separated by each row
         units = UnitDataString.Split(';');
-
-
-        print(GetDataValue(units[0], index2));
+        GetStatsOfCurrentUnit();
+        //print(GetDataValue(units[0], index2));
     }
 
     string GetDataValue(string data, string index)
@@ -40,5 +61,36 @@ public class DataLoader : MonoBehaviour
         string value = data.Substring(data.IndexOf(index) + index.Length);
         value = value.Remove(value.IndexOf("|"));
         return value;
+    }
+
+    void GetStatsOfCurrentUnit()
+    {
+        print("Ran this method");
+        //what is the current unit?
+        name = gameObject.tag;
+        print(name);
+
+        //find which element based on unitName
+        for (int i = 0; i < units.Length-1; i++)
+        {
+            print(GetDataValue(units[i], getName));
+            if (GetDataValue(units[i], getName).Equals(name))
+            {
+                unitName = GetDataValue(units[i], getName);
+                print(getCost.Length + " " + units[i]);
+                cost = int.Parse(GetDataValue(units[i], getCost));
+                synergy = GetDataValue(units[i], getSynergy);
+                synergy2 = GetDataValue(units[i], getSynergy2);
+                HP = int.Parse(GetDataValue(units[i], getHP));
+                mana = int.Parse(GetDataValue(units[i], getMana));
+                ability = GetDataValue(units[i], getAbility);
+                attackSpeed = int.Parse(GetDataValue(units[i], getAttackSpeed));
+                damage = int.Parse(GetDataValue(units[i], getDamage));
+                armor = int.Parse(GetDataValue(units[i], getArmor));
+            }
+        }
+        print("found Element based on name");
+
+
     }
 }
