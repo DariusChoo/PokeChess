24447d85c848ce52db5564bbe9d090a18e03548a
DariusChoo <dariuschoo@gmail.com>
2019-08-31 00:54:09 +0800

    w
    w

--- Pokemon AutoChess/Assets/Scripts/MainMenu.cs
+++ Pokemon AutoChess/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,11 @@ public class MainMenu : MonoBehaviour
 {
     //Loads Register scene
     //Indexing can be seen in Build settings -> Scenes to build
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void GoToRegister()
     {
         SceneManager.LoadScene(1);
